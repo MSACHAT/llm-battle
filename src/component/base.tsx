fde70fbf9468b6979120da4d701417c40272a377
scrod <xueyuqi@bytedance.com>
2024-05-23 11:36:39 +0800

    feat : add user avatar

--- src/component/base.tsx
+++ src/component/base.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Dropdown, Nav } from "@douyinfe/semi-ui";
+import { Avatar, Dropdown, Nav, Space } from "@douyinfe/semi-ui";
 import { Link, Outlet } from "react-router-dom";
 import AIIcon from "./navigation-header-logo.svg";
 import RuleIcon from "./icon-rules.svg";
@@ -32,7 +32,24 @@ export const NavigationBar = () => {
           { itemKey: "leaderBoard", text: "排行榜" },
           { itemKey: "chat", text: "单聊" },
         ]}
-        footer={<img src={RuleIcon} style={{ width: 32, height: 32 }} />}
+        footer={
+          <Space>
+            <img src={RuleIcon} style={{ width: 32, height: 32 }} />
+            <Dropdown
+              position="bottomRight"
+              render={
+                <Dropdown.Menu>
+                  <Dropdown.Item>退出</Dropdown.Item>
+                </Dropdown.Menu>
+              }
+            >
+              <Avatar size={"small"} style={{ margin: 4 }}>
+                U
+              </Avatar>
+              <span>用户1</span>
+            </Dropdown>
+          </Space>
+        }
       />
       <Outlet />
     </div>
