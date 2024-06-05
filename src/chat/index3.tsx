57fab86473b02ca5437b2647161e00a02a66c2d4
scrod <xueyuqi@bytedance.com>
2024-06-05 15:38:10 +0800

    feat : init message content

---
+++ src/chat/index3.tsx
@@ -0,0 +1,231 @@
+/* eslint-disable no-console */
+import { FC, useContext, useEffect, useState } from "react";
+import MessageBox from "@components/MessageBox";
+import { Message, ReactSetState } from "@interfaces";
+import GlobalContext from "@contexts/global";
+import { hasMathJax, initMathJax, renderMaxJax } from "@utils/markdown";
+import { hasMath } from "@utils";
+import { midjourneyConfigs } from "@configs";
+import {
+  type MessageItem,
+  type MessageType,
+  isInProgress,
+  getHashFromCustomId,
+} from "midjourney-fetch";
+import { updateComponentStatus } from "@utils/midjourney";
+import MessageInput from "./MessageInput";
+import ContentHeader from "./ContentHeader";
+
+interface ContentProps {
+  setActiveSetting: ReactSetState<boolean>;
+}
+
+const Content: FC<ContentProps> = ({ setActiveSetting }) => {
+  // input text
+  const [text, setText] = useState("");
+  const [streamMessageMap, setStreamMessageMap] = useState<
+    Record<string, string>
+  >({});
+  const [loadingMap, setLoadingMap] = useState<Record<string, boolean>>({});
+
+  // prompt
+  const [showPrompt, setShowPrompt] = useState(false);
+
+  // controller
+  const [controller, setController] = useState<AbortController>(null);
+
+  const conversation = conversations[currentId];
+  const messages = conversation?.messages ?? [];
+  const mode = conversation?.mode ?? "text";
+  const streamMessage = streamMessageMap[currentId] ?? "";
+  const loading = loadingMap[currentId];
+
+  useEffect(() => {
+    // lazyload, init mathJax when hasMath
+    if (messages.some((message) => hasMath(message.content))) {
+      initMathJax().then(() => renderMaxJax());
+    }
+  }, [messages]);
+
+  // pre initialization
+  useEffect(() => {
+    if (hasMathJax()) return;
+    if (hasMath(streamMessage)) {
+      initMathJax();
+    }
+  }, [streamMessage]);
+
+  const updateMessages = (msgs: Message[]) => {
+    setConversations((msg) => ({
+      ...msg,
+      [currentId]: {
+        ...conversations[currentId],
+        updatedAt: msgs.slice(-1)?.[0]?.createdAt,
+        messages: msgs,
+        // If no title, set the first content
+        title: conversations[currentId].title || msgs[0].content,
+      },
+    }));
+  };
+
+  const sendTextChatMessages = async (content: string) => {
+    const current = currentId;
+    // temp stream message
+    let tempMessage = "";
+    const input: Message[] = [
+      {
+        role: "user",
+        content,
+        createdAt: Date.now(),
+      },
+    ];
+    const allMessages: Message[] = messages.concat(input);
+    updateMessages(allMessages);
+    setText("");
+    setLoadingMap((map) => ({
+      ...map,
+      [current]: true,
+    }));
+    try {
+      const abortController = new AbortController();
+      setController(abortController);
+      const res = await fetch("/api/completions", {
+        method: "POST",
+        body: JSON.stringify({
+          key: configs.openAIApiKey,
+          model: configs.model,
+          messages: configs.continuous
+            ? allMessages.slice(-1 * (configs.messagesCount ?? 4) - 1)
+            : input,
+          temperature: configs.temperature ?? 1,
+          password: configs.password,
+        }),
+        signal: abortController.signal,
+      });
+      if (res.status < 400 && res.ok) {
+        const stream = res.body;
+        const reader = stream.getReader();
+        const decoder = new TextDecoder();
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+          const { value, done } = await reader.read();
+          if (value) {
+            const char = decoder.decode(value);
+            if (char === "\n" && tempMessage.endsWith("\n")) {
+              continue;
+            }
+            if (char) {
+              tempMessage += char;
+              // eslint-disable-next-line no-loop-func
+              setStreamMessageMap((map) => ({
+                ...map,
+                [current]: tempMessage,
+              }));
+            }
+          }
+          if (done) {
+            break;
+          }
+        }
+        updateMessages(
+          allMessages.concat([
+            {
+              role: "assistant",
+              content: tempMessage,
+              createdAt: Date.now(),
+            },
+          ]),
+        );
+        setStreamMessageMap((map) => ({
+          ...map,
+          [current]: "",
+        }));
+        tempMessage = "";
+      } else {
+        const { msg, error } = await res.json();
+        updateMessages(
+          allMessages.concat([
+            {
+              role: "assistant",
+              content: `[${res.status}]Error: ${
+                msg || error?.message || res.statusText || "Unknown"
+              }`,
+              createdAt: Date.now(),
+            },
+          ]),
+        );
+      }
+    } catch (e) {
+      // abort manually or not
+      if (!tempMessage) {
+        updateMessages(
+          allMessages.concat([
+            {
+              role: "assistant",
+              content: `Error: ${e.message || e.stack || e}`,
+              createdAt: Date.now(),
+            },
+          ]),
+        );
+      }
+    } finally {
+      setController(null);
+      setLoadingMap((map) => ({
+        ...map,
+        [current]: false,
+      }));
+    }
+  };
+
+  const stopGenerate = () => {
+    controller?.abort?.();
+    if (streamMessage) {
+      updateMessages(
+        messages.concat([
+          {
+            role: "assistant",
+            content: streamMessage,
+            createdAt: Date.now(),
+          },
+        ]),
+      );
+      setStreamMessageMap((map) => ({
+        ...map,
+        [currentId]: "",
+      }));
+    }
+  };
+
+  return (
+    <div className="flex flex-col h-full w-full">
+      <ContentHeader
+        conversation={conversation}
+        setActiveSetting={setActiveSetting}
+        setShowPrompt={setShowPrompt}
+        setText={setText}
+      />
+      <div className="flex-1 overflow-auto common-scrollbar p-5 pb-0">
+        <MessageBox
+          streamMessage={streamMessage}
+          messages={messages}
+          mode={mode}
+          loading={loading}
+        />
+      </div>
+      <MessageInput
+        text={text}
+        setText={setText}
+        streamMessage={streamMessage}
+        showPrompt={showPrompt && mode !== "image"}
+        setShowPrompt={setShowPrompt}
+        onSubmit={async (message: string) => {
+          sendTextChatMessages(message);
+        }}
+        onCancel={stopGenerate}
+        loading={loading}
+      />
+    </div>
+  );
+};
+
+export default Content;
