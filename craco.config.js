c5d8227f4c6441818cf5878448f5815cd417a94a
scrod <xueyuqi@bytedance.com>
2024-06-13 20:58:09 +0800

    feat: add environment

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { webpack } = require("@storybook/react-webpack5/preset");
 const SemiWebpackPlugin = require("@douyinfe/semi-webpack-plugin").default;
 
 module.exports = {
@@ -11,6 +12,12 @@ module.exports = {
       new SemiWebpackPlugin({
         theme: "@semi-bot/semi-theme-pk",
         include: "~@semi-bot/semi-theme-pk/scss/local.scss",
+      }),
+      // 定义环境变量
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(
+          process.env.NODE_ENV || "development",
+        ),
       }),
     ],
   },
--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,6 +1,19 @@
+export function getBaseUrl() {
+  let baseUrl = "";
+
+  // 检查当前环境
+  if (process.env.NODE_ENV === "development") {
+    // 在开发环境下使用localhost
+    baseUrl = "http://172.10.21.42:8087";
+  } else {
+    // 在生产环境下使用不同的URL
+    baseUrl = "http://43.138.41.24:8085";
+  }
+
+  return baseUrl;
+}
 const config = {
-  // apiUrl: "http://43.138.41.24:8085",
-  apiUrl: "http://172.10.21.42:8087",
+  apiUrl: getBaseUrl,
 };
 
 export default config;
