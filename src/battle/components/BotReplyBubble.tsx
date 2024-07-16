cd697256627c26c4e05874f68c9ced388884e6dc
StanH123h <1735443634@qq.com>
2024-07-16 13:17:21 +0800

    fix (battle/index,battle/BotReplyBubble):修复光标闪烁效果，修复气泡的padding

--- src/battle/components/BotReplyBubble.tsx
+++ src/battle/components/BotReplyBubble.tsx
@@ -2,11 +2,10 @@ import { Spin } from "@douyinfe/semi-ui";
 import "./BotReplyBubble.scss";
 import { marked } from "marked";
 import hljs from "highlight.js";
-import React from "react";
-
-const BlinkingCursor: React.FC = () => {
-  return <div className="cursor"></div>;
-};
+import React, { useState } from "react";
+import { Simulate } from "react-dom/test-utils";
+import load = Simulate.load;
+import { set } from "lodash-es";
 
 function renderMessageContent(msg: any) {
   marked.setOptions({
@@ -39,6 +38,12 @@ export const BotReply = ({
   content: string;
   loading?: boolean;
 }) => {
+  const [blink, setBlink] = useState(false);
+  setInterval(() => {
+    if (loading) {
+      setBlink(!blink);
+    }
+  }, 500);
   return (
     <div className={"bot-reply"}>
       <div className={"bot-chat-bubble"}>
@@ -46,8 +51,9 @@ export const BotReply = ({
           <Spin size={"middle"} style={{ marginBottom: -8 }} />
         ) : (
           <>
-            {renderMessageContent(content) || "未知错误"}
-            {loading ? <BlinkingCursor /> : ""}
+            {renderMessageContent(
+              content + (loading && blink ? "▌" : "") || "未知错误",
+            )}
           </>
         )}
       </div>
